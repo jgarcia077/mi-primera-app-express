a712eeaaab4055aa71cc5676f7716d0a542873c2
Jhonatan <ja.garcia02@ciaf.edu.co>
2025-09-24 21:04:17 -0500

    Primer commit

---
+++ Data/products.js
@@ -0,0 +1,603 @@
+let products = [{
+  "id": 1,
+  "name": "Ice Cream Maker",
+  "price": "$85.5M",
+  "category": "Kitchen",
+  "stock": 77
+}, {
+  "id": 2,
+  "name": "Golf Putting Green",
+  "price": "$188.28M",
+  "category": "Sports",
+  "stock": 13
+}, {
+  "id": 3,
+  "name": "Kale Caesar Salad Kit",
+  "price": "$2.18B",
+  "category": "Food - Salads",
+  "stock": 52
+}, {
+  "id": 4,
+  "name": "Rain Jacket",
+  "price": "$333.23M",
+  "category": "Clothing",
+  "stock": 43
+}, {
+  "id": 5,
+  "name": "Halloween Decoration Set",
+  "price": "$6.26B",
+  "category": "Home",
+  "stock": 21
+}, {
+  "id": 6,
+  "name": "Portable Bluetooth Speaker",
+  "price": "$156.02B",
+  "category": "Audio",
+  "stock": 55
+}, {
+  "id": 7,
+  "name": "Noise Cancelling Ear Muffs",
+  "price": "$13.2M",
+  "category": "Safety",
+  "stock": 25
+}, {
+  "id": 8,
+  "name": "Home Delivery Food Journal",
+  "price": "$92.42M",
+  "category": "Health",
+  "stock": 71
+}, {
+  "id": 9,
+  "name": "Car Windshield Sun Shade",
+  "price": "$46.6B",
+  "category": "Automotive",
+  "stock": 85
+}, {
+  "id": 10,
+  "name": "Sweet Potato Fries",
+  "price": "$159.18M",
+  "category": "Food - Frozen Foods",
+  "stock": 88
+}, {
+  "id": 11,
+  "name": "Gluten-Free Pancake Mix",
+  "price": "$39.32M",
+  "category": "Food - Baking Goods",
+  "stock": 67
+}, {
+  "id": 12,
+  "name": "Balsamic Glazed Brussels Sprouts",
+  "price": "$152.9M",
+  "category": "Food - Vegetables",
+  "stock": 53
+}, {
+  "id": 13,
+  "name": "Teriyaki Chicken Stir-Fry",
+  "price": "$379.39M",
+  "category": "Food - Frozen Foods",
+  "stock": 31
+}, {
+  "id": 14,
+  "name": "Magnetic Spice Containers",
+  "price": "$10.06M",
+  "category": "Kitchen",
+  "stock": 50
+}, {
+  "id": 15,
+  "name": "Electric Butter Churn",
+  "price": "$887.04M",
+  "category": "Kitchen",
+  "stock": 18
+}, {
+  "id": 16,
+  "name": "Roasted Sweet Corn",
+  "price": "$716.78M",
+  "category": "Food - Frozen",
+  "stock": 43
+}, {
+  "id": 17,
+  "name": "Breezy Off-The-Shoulder Top",
+  "price": "$226.66M",
+  "category": "Clothing - Tops",
+  "stock": 68
+}, {
+  "id": 18,
+  "name": "Californian Raisins",
+  "price": "$480.69M",
+  "category": "Food - Dried Fruits",
+  "stock": 81
+}, {
+  "id": 19,
+  "name": "Inflatable Pool Float",
+  "price": "$104.92M",
+  "category": "Outdoor",
+  "stock": 56
+}, {
+  "id": 20,
+  "name": "Dog Waste Bag Dispenser",
+  "price": "$27.57M",
+  "category": "Pets",
+  "stock": 29
+}, {
+  "id": 21,
+  "name": "Vegetable Broth",
+  "price": "$145.98B",
+  "category": "Food - Canned Goods",
+  "stock": 95
+}, {
+  "id": 22,
+  "name": "Tailgating Set",
+  "price": "$2.31M",
+  "category": "Outdoor",
+  "stock": 88
+}, {
+  "id": 23,
+  "name": "Mini Electric Kettle",
+  "price": "n/a",
+  "category": "Kitchen",
+  "stock": 38
+}, {
+  "id": 24,
+  "name": "Air Fryer Oven",
+  "price": "$2.85B",
+  "category": "Kitchen",
+  "stock": 88
+}, {
+  "id": 25,
+  "name": "Caramelized Onion Dip",
+  "price": "$35.52M",
+  "category": "Food - Dairy",
+  "stock": 91
+}, {
+  "id": 26,
+  "name": "Couscous",
+  "price": "n/a",
+  "category": "Food - Grains",
+  "stock": 24
+}, {
+  "id": 27,
+  "name": "Low-Fat Cottage Cheese",
+  "price": "$4.89B",
+  "category": "Food - Dairy",
+  "stock": 74
+}, {
+  "id": 28,
+  "name": "Pet Travel Bowl",
+  "price": "$1.2B",
+  "category": "Pets",
+  "stock": 28
+}, {
+  "id": 29,
+  "name": "Honey Lemon Tea",
+  "price": "$7.3B",
+  "category": "Food - Beverages",
+  "stock": 23
+}, {
+  "id": 30,
+  "name": "Aeropress Coffee Maker",
+  "price": "$172.67M",
+  "category": "Kitchen",
+  "stock": 42
+}, {
+  "id": 31,
+  "name": "Electric Skillet",
+  "price": "$26.03M",
+  "category": "Kitchen",
+  "stock": 54
+}, {
+  "id": 32,
+  "name": "LED Makeup Mirror",
+  "price": "$229.33M",
+  "category": "Beauty",
+  "stock": 88
+}, {
+  "id": 33,
+  "name": "Car Sunshade",
+  "price": "$13.85M",
+  "category": "Automotive",
+  "stock": 87
+}, {
+  "id": 34,
+  "name": "Coconut Chia Seed Pudding",
+  "price": "$430.9M",
+  "category": "Food - Snacks",
+  "stock": 50
+}, {
+  "id": 35,
+  "name": "Whole Grain Cereal",
+  "price": "$17.78B",
+  "category": "Food - Breakfast",
+  "stock": 19
+}, {
+  "id": 36,
+  "name": "Organic Tomato Ketchup",
+  "price": "$19.79M",
+  "category": "Food - Condiments",
+  "stock": 94
+}, {
+  "id": 37,
+  "name": "Electric Griddle with Lid",
+  "price": "$3.17B",
+  "category": "Kitchen",
+  "stock": 33
+}, {
+  "id": 38,
+  "name": "Compressed Towel Tablets",
+  "price": "$536.16M",
+  "category": "Travel",
+  "stock": 46
+}, {
+  "id": 39,
+  "name": "Coconut Cream",
+  "price": "$48.88B",
+  "category": "Food - Baking & Cooking",
+  "stock": 52
+}, {
+  "id": 40,
+  "name": "Natural Peanut Butter",
+  "price": "n/a",
+  "category": "Food - Spreads",
+  "stock": 14
+}, {
+  "id": 41,
+  "name": "Gardening Tool Belt",
+  "price": "$38.81M",
+  "category": "Garden",
+  "stock": 95
+}, {
+  "id": 42,
+  "name": "Beef Stroganoff Mix",
+  "price": "$2.2B",
+  "category": "Food - Meal Kits",
+  "stock": 12
+}, {
+  "id": 43,
+  "name": "Basil Lemonade",
+  "price": "n/a",
+  "category": "Food - Beverages",
+  "stock": 99
+}, {
+  "id": 44,
+  "name": "Samoas Cookie Mix",
+  "price": "$64.14M",
+  "category": "Food - Baking",
+  "stock": 17
+}, {
+  "id": 45,
+  "name": "Smart Air Purifier",
+  "price": "$790.37M",
+  "category": "Home",
+  "stock": 14
+}, {
+  "id": 46,
+  "name": "Mini Indoor Herb Garden Kit",
+  "price": "n/a",
+  "category": "Garden",
+  "stock": 99
+}, {
+  "id": 47,
+  "name": "Hibiscus Tea Bags",
+  "price": "$2.43B",
+  "category": "Food - Beverages",
+  "stock": 47
+}, {
+  "id": 48,
+  "name": "Outdoor Folding Table",
+  "price": "$3.1B",
+  "category": "Outdoor",
+  "stock": 100
+}, {
+  "id": 49,
+  "name": "Collapsible Camping Cup",
+  "price": "$8.05B",
+  "category": "Outdoor",
+  "stock": 48
+}, {
+  "id": 50,
+  "name": "Organic Vanilla Bean Ice Cream",
+  "price": "$1.11B",
+  "category": "Food - Frozen Desserts",
+  "stock": 51
+}, {
+  "id": 51,
+  "name": "Pet Hair Vacuum Attachment",
+  "price": "$7.07M",
+  "category": "Pets",
+  "stock": 99
+}, {
+  "id": 52,
+  "name": "Digital Bullet Journal",
+  "price": "$88.09B",
+  "category": "Apps",
+  "stock": 74
+}, {
+  "id": 53,
+  "name": "Kale Caesar Salad Kit",
+  "price": "$7.06B",
+  "category": "Food - Salads",
+  "stock": 81
+}, {
+  "id": 54,
+  "name": "Hot Dog Buns",
+  "price": "$58.2M",
+  "category": "Food - Bakery",
+  "stock": 66
+}, {
+  "id": 55,
+  "name": "Grilled Vegetable Medley",
+  "price": "$7.48B",
+  "category": "Food - Vegetables",
+  "stock": 65
+}, {
+  "id": 56,
+  "name": "Indestructible Dog Toy",
+  "price": "$76.6B",
+  "category": "Pets",
+  "stock": 85
+}, {
+  "id": 57,
+  "name": "Frozen Hash Browns",
+  "price": "$142.06M",
+  "category": "Food - Frozen Foods",
+  "stock": 59
+}, {
+  "id": 58,
+  "name": "Thyme",
+  "price": "$1.09B",
+  "category": "Food - Fresh Produce",
+  "stock": 21
+}, {
+  "id": 59,
+  "name": "Stainless Steel Cutlery Set",
+  "price": "$695.78M",
+  "category": "Kitchen",
+  "stock": 30
+}, {
+  "id": 60,
+  "name": "Scent Diffuser Oil",
+  "price": "$76.04M",
+  "category": "Home",
+  "stock": 55
+}, {
+  "id": 61,
+  "name": "Fleece Lined Leggings",
+  "price": "n/a",
+  "category": "Clothing",
+  "stock": 70
+}, {
+  "id": 62,
+  "name": "Outdoor Adventure Kit",
+  "price": "$2.13B",
+  "category": "Outdoor",
+  "stock": 97
+}, {
+  "id": 63,
+  "name": "Aeropress Coffee Maker",
+  "price": "$92.98M",
+  "category": "Kitchen",
+  "stock": 99
+}, {
+  "id": 64,
+  "name": "Fleece Hoodie",
+  "price": "n/a",
+  "category": "Clothing - Outerwear",
+  "stock": 100
+}, {
+  "id": 65,
+  "name": "High-Waisted Skirt",
+  "price": "$111.49M",
+  "category": "Clothing - Bottoms",
+  "stock": 71
+}, {
+  "id": 66,
+  "name": "Juice Extractor",
+  "price": "$415.84M",
+  "category": "Kitchen",
+  "stock": 19
+}, {
+  "id": 67,
+  "name": "Sweet Potato & Kale Hash",
+  "price": "$200M",
+  "category": "Food - Prepared Foods",
+  "stock": 31
+}, {
+  "id": 68,
+  "name": "Teriyaki Stir-Fry Sauce",
+  "price": "$9.89B",
+  "category": "Food - Condiments",
+  "stock": 39
+}, {
+  "id": 69,
+  "name": "Italian Pasta",
+  "price": "$150.62M",
+  "category": "Food - Grains",
+  "stock": 37
+}, {
+  "id": 70,
+  "name": "Vintage Graphic Tee",
+  "price": "$7.13B",
+  "category": "Clothing - Tops",
+  "stock": 68
+}, {
+  "id": 71,
+  "name": "Cookbook",
+  "price": "$1.5B",
+  "category": "Books",
+  "stock": 13
+}, {
+  "id": 72,
+  "name": "Veggie Burger Patties",
+  "price": "$1.83B",
+  "category": "Food - Meat Substitutes",
+  "stock": 30
+}, {
+  "id": 73,
+  "name": "Chewy Granola Bars",
+  "price": "$1.05B",
+  "category": "Food - Snacks",
+  "stock": 87
+}, {
+  "id": 74,
+  "name": "Portable Speaker Stand",
+  "price": "$5.06B",
+  "category": "Audio",
+  "stock": 63
+}, {
+  "id": 75,
+  "name": "Set of Silicone Cooking Utensils",
+  "price": "$7.35B",
+  "category": "Kitchen",
+  "stock": 70
+}, {
+  "id": 76,
+  "name": "Digital Food Scale",
+  "price": "$1.54B",
+  "category": "Kitchen",
+  "stock": 77
+}, {
+  "id": 77,
+  "name": "Decorative Throw Pillows",
+  "price": "$2.68B",
+  "category": "Home",
+  "stock": 81
+}, {
+  "id": 78,
+  "name": "Black Bean Soup",
+  "price": "$5.31B",
+  "category": "Food - Soups",
+  "stock": 57
+}, {
+  "id": 79,
+  "name": "Deep Tissue Massage Gun",
+  "price": "$1.11B",
+  "category": "Health",
+  "stock": 79
+}, {
+  "id": 80,
+  "name": "Outdoor Sports Backpack",
+  "price": "$2.89B",
+  "category": "Outdoor",
+  "stock": 86
+}, {
+  "id": 81,
+  "name": "Chicken Sausage",
+  "price": "$2.18B",
+  "category": "Food - Meat",
+  "stock": 56
+}, {
+  "id": 82,
+  "name": "Zesty Garlic Marinade",
+  "price": "$201.19M",
+  "category": "Food - Condiments",
+  "stock": 52
+}, {
+  "id": 83,
+  "name": "Margherita Pizza",
+  "price": "$120.61M",
+  "category": "Food - Frozen Foods",
+  "stock": 49
+}, {
+  "id": 84,
+  "name": "Hibiscus Tea Bags",
+  "price": "n/a",
+  "category": "Food - Beverages",
+  "stock": 45
+}, {
+  "id": 85,
+  "name": "Leather Wallet",
+  "price": "n/a",
+  "category": "Accessories",
+  "stock": 35
+}, {
+  "id": 86,
+  "name": "Spiralizer",
+  "price": "$171.9M",
+  "category": "Kitchen",
+  "stock": 83
+}, {
+  "id": 87,
+  "name": "Sweet Chili Thai Sauce",
+  "price": "$187.18M",
+  "category": "Food - Condiments",
+  "stock": 23
+}, {
+  "id": 88,
+  "name": "Garlic Herb Marinade",
+  "price": "$21.03M",
+  "category": "Food - Marinades",
+  "stock": 15
+}, {
+  "id": 89,
+  "name": "Aeropress Coffee Maker",
+  "price": "n/a",
+  "category": "Kitchen",
+  "stock": 98
+}, {
+  "id": 90,
+  "name": "Ergonomic Gaming Chair",
+  "price": "$1.11B",
+  "category": "Gaming",
+  "stock": 94
+}, {
+  "id": 91,
+  "name": "Coconut Curry Lentil Soup",
+  "price": "$31.83M",
+  "category": "Food - Canned Goods",
+  "stock": 95
+}, {
+  "id": 92,
+  "name": "Phone Screen Protector",
+  "price": "n/a",
+  "category": "Accessories",
+  "stock": 23
+}, {
+  "id": 93,
+  "name": "Compact Digital Camera",
+  "price": "$58.82B",
+  "category": "Photography",
+  "stock": 54
+}, {
+  "id": 94,
+  "name": "Floral Wrap Top",
+  "price": "$261.62M",
+  "category": "Clothing - Tops",
+  "stock": 81
+}, {
+  "id": 95,
+  "name": "Vegan Caesar Dressing",
+  "price": "$4.06B",
+  "category": "Food - Condiments",
+  "stock": 89
+}, {
+  "id": 96,
+  "name": "Wrap Jumpsuit",
+  "price": "$332.64M",
+  "category": "Clothing - Jumpsuits",
+  "stock": 73
+}, {
+  "id": 97,
+  "name": "Organic Black Beans",
+  "price": "$42.84B",
+  "category": "Food - Canned Goods",
+  "stock": 70
+}, {
+  "id": 98,
+  "name": "Organic Black Bean Burger",
+  "price": "$687.82M",
+  "category": "Food - Frozen Foods",
+  "stock": 91
+}, {
+  "id": 99,
+  "name": "Garlic Naan Bread",
+  "price": "n/a",
+  "category": "Food - Bakery",
+  "stock": 77
+}, {
+  "id": 100,
+  "name": "Curried Lentil Salad",
+  "price": "$128.18M",
+  "category": "Food - Salads",
+  "stock": 82
+}]
+
+module.exports = products;
--- index.js
+++ index.js
@@ -1,35 +1,23 @@
 console.log("Mi primera app en express");
 require('dotenv').config();
 const express = require('express');
+const products = require('./Data/products');
 const app = express();
 const PORT = process.env.PORT;
-const products = [
-    {
-        id: 1,
-        name: 'Producto 1',
-        price: 100
-    },
-    {
-        id: 2,
-        name: 'Producto 2',
-        price: 200
-    },
-    {
-        id: 3,
-        name: 'Producto 3',
-        price: 300
-    }
-]
+
 app.use(express.json());
 
+// endpoints
+// http://localhost:3000/
 app.get('/', (req, res) => {
     res.json({
-        message: 'Productos',
+        message: '¡Hola! Express funcionando',
         timestamp: new Date().toISOString(),
         status: 'success',
     })
 });
 
+// http://localhost:3000/products
 app.get('/products', (req, res) => {
     res.json({
         message: 'Productos',
